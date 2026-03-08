2e40b4777107fafca4b3195c57fbc17b1612125c
Yashas <yash8narayan@gmail.com>
2026-03-08 14:15:48 +0000

    Preceding changes

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
+      <WalletProvider>
       <PlayerProvider>
         <Toaster />
         <Sonner />
