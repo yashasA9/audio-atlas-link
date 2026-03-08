896465d89bb75e04eab42f73b97f31a693e00911
Yashas <yash8narayan@gmail.com>
2026-03-08 14:15:47 +0000

    Preceding changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PlayerProvider } from "@/context/PlayerContext";
+import { WalletProvider } from "@/context/WalletContext";
 import { MainLayout } from "@/components/MainLayout";
 import LandingPage from "@/pages/LandingPage";
 import HomePage from "@/pages/HomePage";
